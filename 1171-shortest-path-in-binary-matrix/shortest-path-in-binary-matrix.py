bf0ac84c60be170dca5792acbd46f320e64bd9c9
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2026-09-11 10:23:44 -0500

    Time: 222 ms (19.92%) | Memory: 20.7 MB (40.23%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -1,29 +1,26 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        if grid[0][0] == 1:
-            return -1
+        # bfs through the thing, only adding to the queue cells that are 0
+        # then we you reach the final, perform min operaiton with current min and the current value
 
-        def is_valid(i, j):
-            return i >= 0 and j >= 0 and i < len(grid) and j < len(grid[0]) and grid[i][j] == 0
+        queue = deque([(0,0,1)])
+        visited = {(0,0)}
         
-        n = len(grid)
-        seen = {(0, 0)}
-        q = deque([(0, 0, 1)])
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, -1), (-1, 1), (-1, -1), (1, 1)]
-        while q:
-            top = q.popleft()
-            if top[0] == (n - 1) and top[1] == (n - 1):
-                return top[2]
-            
-            # iterate through all the directions and push them onto the queue
+        min_path_length = float('inf')
+
+        directions = [(0,1),(1,0),(-1,0),(0,-1),(1,1),(-1,1),(1,-1),(-1,-1)]
+
+        while queue:
+            row, col, curr_path_length = queue.popleft()
+            if grid[row][col] == 1:
+                return -1
+            if row == len(grid) - 1 and col == len(grid[0]) - 1:
+                min_path_length = min(min_path_length, curr_path_length)
+
             for dx, dy in directions:
-                new_row = dx + top[0]
-                new_col = dy + top[1]
-
-                if is_valid(new_row, new_col) and (new_row, new_col) not in seen:
-                    seen.add((new_row, new_col))
-                    q.append((new_row, new_col, top[2] + 1))
-        return -1
-
-
-            
+                new_row = dx + row
+                new_col = dy + col
+                if 0 <= new_row < len(grid) and 0 <= new_col < len(grid[0]) and grid[new_row][new_col] == 0 and (new_row, new_col) not in visited:
+                    queue.append((new_row, new_col, curr_path_length + 1))
+                    visited.add((new_row, new_col))
+        return min_path_length if min_path_length != float('inf') else -1
